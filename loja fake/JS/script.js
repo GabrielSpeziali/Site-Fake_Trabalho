19d562e4ea8c1917311867e6aa07955df3162f57
Gabriel Speziali <gg275403@gmail.com>
2025-04-30 20:53:32 -0300

    Background and products

---
+++ loja fake/JS/script.js
@@ -0,0 +1,13 @@
+function scrollToLeft(id) {
+    const el = document.getElementById('productScroll');
+    el.scrollBy({ left: -300, behavior: 'smooth' });
+  }
+  
+  function scrollToRight(id) {
+    const el = document.getElementById('productScroll');
+    el.scrollBy({ left: 300, behavior: 'smooth' });
+  }
+  
+  
+  
+  
